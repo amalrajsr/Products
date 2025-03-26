ef922e0c75ef44f851b641915f342ed6ce8c5e38
amalrajsr <sramalraj123@gmail.com>
2025-03-26 23:51:32 +0530

    remove app.css file

--- src/api/fetchProducts.js
+++ src/api/fetchProducts.js
@@ -3,10 +3,10 @@ import axios from "axios";
 export const fetchProducts = async ({ pageParam = 1, query = "" }) => {
   const limit = 15;
   const response = await axios.get(
-    `/api/products?limit=${limit}&page=${pageParam}`
+    `https://fakestoreapi.in/api/products?limit=${limit}&page=${pageParam}`
   );
   const products = response.data.products;
-  // If a query is provided, filter the products by title (case-insensitive)
+  // If a query is provided, filter the products by title
   const filteredProducts = query
     ? products.filter((product) =>
         product.title.toLowerCase().includes(query.toLowerCase())
