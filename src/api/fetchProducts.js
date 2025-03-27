0fe22948119ef93f937664e428f9552f404dcf72
amalrajsr <sramalraj123@gmail.com>
2025-03-27 09:16:05 +0530

    refactor api fetching

--- src/api/fetchProducts.js
+++ src/api/fetchProducts.js
@@ -1,20 +1,28 @@
 import axios from "axios";
 
 export const fetchProducts = async ({ pageParam = 1, query = "" }) => {
-  const limit = 15;
-  const response = await axios.get(
-    `https://fakestoreapi.in/api/products?limit=${limit}&page=${pageParam}`
-  );
-  const products = response.data.products;
-  // If a query is provided, filter the products by title
-  const filteredProducts = query
-    ? products.filter((product) =>
-        product.title.toLowerCase().includes(query.toLowerCase())
-      )
-    : products;
-  return {
-    products: filteredProducts,
-    nextPage: pageParam + 1,
-    hasMore: products.length === limit,
-  };
+  try {
+    const limit = 15;
+    const response = await axios.get(
+      `https://fakestoreapi.in/api/products?limit=${limit}&page=${pageParam}`
+    );
+    const products = response.data.products;
+    // If a query is provided, filter the products by title
+    const filteredProducts = query
+      ? products.filter((product) =>
+          product.title.toLowerCase().includes(query.toLowerCase())
+        )
+      : products;
+    return {
+      products: filteredProducts,
+      nextPage: pageParam + 1,
+      hasMore: products.length === limit,
+    };
+  } catch (err) {
+    return {
+      products: [],
+      nextPage: pageParam + 1,
+      hasMore: false,
+    };
+  }
 };
